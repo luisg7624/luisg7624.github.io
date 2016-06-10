f3050aacdef4d2888655890fa4b7389796f63c71
luisg7624 <luisg7624@kippnyccp.org>
2016-06-10 12:48:14 -0400

    Update application.js

--- application.js
+++ application.js
@@ -2,7 +2,10 @@ $(document).ready(function(){
   $("#a_m_header").click(function(){
     $("#a_m_par").css("display","block");
   });
-  $("#").click(function(){
-    $("#").css("display","block");
+  $("#h_i_header").click(function(){
+    $("#h_i_par").css("display","block");
   });
+  $("#f_i_header").click(function(){
+    
+  })
 })
